acba19bd33bf8584f382aa0be647bd1a0d453b76
Katja Mathesius <katmathematics@gmail.com>
2024-02-13 19:24:24 -0600

    Partial push of NCEI Pulling
    A non-functioning push of the main body of the code for pulling lightning data

--- data_scraping/GetData.py
+++ data_scraping/GetData.py
@@ -5,12 +5,18 @@
 
 from bs4 import BeautifulSoup # For scraping data from web contents
 from urllib.request import urlopen # For grabbing data from the web
+from urllib.request import urlretrieve # For grabbing data from the web
 import ssl # For fixing a connection issue w/ the web scraper
 import datetime # For data that can reliably be grabbed up to a current data, this is used to keep the date current
 import pandas as pd # For saving data to a csv format
 import re # For file
 import os # For saving the file to a specific directory
 import sys # For taking variables from the command line
+import gzip # For unzipping gzip
+
+
+# import for counting the rows real quick
+import glob 
 
 # Collects data from the Bonneville Power Administration website
 # Ideally we'd like to be able to merge the tables within this step, but for now merge_tables is an unused variable
@@ -61,6 +67,47 @@ def get_bpa_data(merge_tables=False,directory="data\\web_data"):
             SaveDataToCSV(tables_rows,tables_headers,filename)  
                    
     return "BPA data was accquired successfully!"
+
+# Gets data about storms from the national centers for environmental information
+def get_ncei_data(directory="data\\web_data\\ncei_data"):
+    START_YEAR_NCEI = 1986
+    BASE_URL_NCEI = "https://www.ncei.noaa.gov/pub/data/swdi/database-csv/v2/"
+    RETRIEVE_FILE_ROOT = "nldn-tiles-"
+
+    # Keeps the loop end at the current year 
+    cur_year = datetime.date.today().year
+
+    # Loops through all the available years on the BPA website to accquire all the data
+    for year in range(START_YEAR_NCEI,cur_year):
+        retrieve_file = RETRIEVE_FILE_ROOT + str(year) + ".csv.gz"
+        url = BASE_URL_NCEI + retrieve_file
+        path, headers = urlretrieve(url, retrieve_file)
+
+        print(headers)
+        print(path)
+
+        with gzip.open(retrieve_file) as f:
+            lines = f.readlines()
+            # move file pointer to the beginning of a file
+            #f.seek(0)
+            #f = f.writelines(lines[1:])
+
+            data = lines[2:]
+
+            data = [str(row).strip('\\n\'\n') for row in data]
+            data = [str(row).lstrip('b\'') for row in data]
+
+            clean_data = [str(row).split(',') for row in data]
+            #print(f.readlines())
+            #features_train = pd.read_csv(data, on_bad_lines='skip')
+            data_headers = clean_data[0]
+            data = clean_data[1:]
+
+            new_file_name = RETRIEVE_FILE.rstrip(".gz")
+            dir = AppendDir(new_file_name,"data\\web_data\\ncei_data")
+            SaveDataToCSV(data,data_headers,dir)
+        os.remove(path)
+    return "NCEI Data was accquired successfully!"
 
 # Appends the desired directory to the filename
 def AppendDir(filename,directory="data\\web_data"):
@@ -127,12 +174,77 @@ def order_soup(url):
     soup = BeautifulSoup(html, features="html.parser")
     return soup
 
+
+def CSV_Count():
+    # use glob to get all the csv files  
+    # in the folder 
+    csv_files = glob.glob(os.path.join(os.pardir, "data\\web_data\\*.csv")) 
+    
+    avg_rows = 0;
+    counter = 0;
+
+    row_count_log = []
+    title_log = []
+
+    customer_ser = 0
+    transmission_line = 0
+    transformer_inter = 0
+
+    # loop over the list of csv files 
+    for f in csv_files: 
+        
+        # read the csv file 
+        df = pd.read_csv(f) 
+        
+        # print the location and filename 
+        #print('File Name:', f.split("\\")[-1]) 
+        
+        # print the content 
+        #print('Row Count:') 
+        #print(df.shape[0])
+        #print() 
+
+        avg_rows += df.shape[0]
+        counter += 1
+        
+        title_log.append(f.split("\\")[-1])
+        row_count_log.append(df.shape[0])
+
+        name =  f.split("\\")[-1]
+        if "customer_service" in name:
+            customer_ser += df.shape[0]
+        elif "transformer_interruptions" in name:
+            transformer_inter += df.shape[0]
+        elif "transmission_line" in name:
+            transmission_line += df.shape[0]
+    
+
+
+    print("Total Row Count: ")
+    print(avg_rows)
+    #print("Avg Row Count: ")
+    #print(avg_rows/counter)
+    print("Rows in Customer Service: ")
+    print(customer_ser)
+    print("Rows in Transformer Interruptions: ")
+    print(transformer_inter)
+    print("Rows in Transmission Line: ")
+    print(transmission_line)
+
+    row_count_log = [row_count_log]
+    df = pd.DataFrame(row_count_log, columns = title_log) # index = false removes the index col from the data
+    df.to_csv("Row_Counts_In_Each_Table.csv")
+
 if __name__ == "__main__":
     
+    #get_ncei_data()
+    #CSV_Count()
     # Pass get_bpa_data a directory if given on the cli, else just run it w/ the default dir
-    if len(sys.argv) > 1:
-        dir = sys.argv[1]
-        dir = dir.replace('\\','\\\\')
-        get_bpa_data(directory=dir)
-    else:
-        get_bpa_data()
+    #if len(sys.argv) > 1:
+    #    dir = sys.argv[1]
+    #    dir = dir.replace('\\','\\\\')
+    #    get_bpa_data(directory=dir)
+    #else:
+    #    get_bpa_data()
+
+    print("Done!")
