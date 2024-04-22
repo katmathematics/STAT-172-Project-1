9418f4b556222ed361d04f3a0d2ee67cfc2741b0
Katja Mathesius <katmathematics@gmail.com>
2024-04-22 04:29:18 -0500

    Wildfire Modeling
    All the modeling code for the wildfire data

--- R/Models/WildfireModelLM.R
+++ R/Models/WildfireModelLM.R
@@ -10,9 +10,16 @@ invisible(lapply(packages, library, character.only = TRUE))
 
 
 # Read in data
-complete_data = read.csv("data/model_data/ModelDataComplete.csv")
-complete_data = select(complete_data, -c("county","Region","flag"))
+complete_data = read.csv("data/cleaned_data/WildfiresClean.csv")
+#complete_data = select(complete_data, -c("flag","states_covered"))
 df <- complete_data
+
+# Read the lightning data's date as a date
+df$date <- as.character(df$date)
+df$date <- paste("01",df$date)
+df$date <- gsub('-', ' ', df$date)
+df$date <- format(as.Date(df$date, format =  '%d %b %y'), '%b %Y')
+str(df)
 
 
 fir_model_df <- df %>%
@@ -25,7 +32,7 @@ for(lag_val in 12:23) {
   fir_model_df[[lag_col_name]] <- dplyr::lag(fir_model_df$sum_wildfires, n=lag_val)
 }
 
-fir_model_clean = fir_model_df[fir_model_df[["date"]] >= "2017-06-01", ]
+fir_model_clean = fir_model_df[fir_model_df[["date"]] >= "1994-03-01", ]
 
 fir_model_train <- fir_model_clean %>% 
   filter(date < "2020-01-01 00:00:00")
@@ -48,16 +55,53 @@ fir_model_test_grp <- fir_model_test %>%
 
 fir_plot_data = select(fir_model_test_grp, c("state","date","sum_wildfires","wildfire_forecast"))
 
-fir_plot_data <- fir_plot_data %>% mutate(absolute_error = abs(abs(sum_wildfires)-abs(wildfire_forecast)))
+# Check the Mean Absolute Error
+fir_plot_data_complete <- fir_plot_data[complete.cases(fir_plot_data), ]
+mae(fir_plot_data_complete$sum_wildfires, fir_plot_data_complete$wildfire_forecast)
+rae(fir_plot_data_complete$sum_wildfires, fir_plot_data_complete$wildfire_forecast)
 
-fir_plot_data = select(fir_plot_data, c("state","date","absolute_error"))
 
-fir_plot_data_short <- fir_plot_data %>%
+
+
+# For condensing the data to plot
+fir_plot_forecast_data_short <- fir_plot_data_complete %>%
   group_by(state) %>%
-  summarize(absolute_error = mean(absolute_error))
+  summarize(wildfire_forecast = sum(wildfire_forecast))
 
-g <- ggplot(lit_plot_data_short) +
-  geom_polygon(aes(fill=absolute_error)) +
-  scale_fill_distiller("water level", palette="Spectral") +
-  ggtitle("Water by State")
-g
+fir_plot_real_data_short <- fir_plot_data_complete %>%
+  group_by(state) %>%
+  summarize(sum_wildfires = sum(sum_wildfires))
+
+# Plot the data
+states_map <- map_data("state")
+ggplot(fir_plot_real_data_short, aes(map_id = state)) + 
+  geom_map(aes(fill = sum_wildfires), map = states_map) +
+  scale_fill_gradientn(colors=c("#0072B2","#000000")) + 
+  expand_limits(x = states_map$long, y = states_map$lat) + 
+  borders("state", colour = "#222222") + 
+  labs(fill='# Wildfires', title = "Real Annual Total Wildfires (Jan 2020-Dec 2020)") +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+
+# Plot the data
+states_map <- map_data("state")
+ggplot(fir_plot_forecast_data_short, aes(map_id = state)) + 
+  geom_map(aes(fill = wildfire_forecast), map = states_map) +
+  scale_fill_gradientn(colors=c("#0072B2","#000000")) + 
+  expand_limits(x = states_map$long, y = states_map$lat) + 
+  borders("state", colour = "#222222") + 
+  labs(fill='# Wildfires', title = "Predicted Annual Total Wildfires (Jan 2020-Dec 2020)") +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+
+# Write the Predictions
+write.csv(fir_plot_data_complete, "data/prediction_data/WildfirePredictionsLM.csv", row.names=FALSE, quote=FALSE)
